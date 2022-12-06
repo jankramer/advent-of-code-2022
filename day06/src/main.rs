9c965aa26c0d6f69303ab873d1aa4dba3070a238
Jan Kramer <jan@jankramer.eu>
2022-12-06 06:23:12 +0100

    Day 06

---
+++ day06/src/main.rs
@@ -0,0 +1,17 @@
+use itertools::Itertools;
+use std::iter::Iterator;
+
+fn main() {
+    let input: Vec<char> = include_str!("input.txt").chars().collect();
+    println!("Part A: {}", solve(&input, 4));
+    println!("Part B: {}", solve(&input, 14));
+}
+
+fn solve(chars: &[char], n: usize) -> usize {
+    chars
+        .windows(n)
+        .enumerate()
+        .find(|(_, xs)| xs.iter().all_unique())
+        .map(|(start_pos, _)| start_pos + n)
+        .unwrap()
+}
