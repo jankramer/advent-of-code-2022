6a6ae03079714201aeba2b8f49b77b37ba48145c
Jan Kramer <jan@jankramer.eu>
2022-12-12 17:35:39 +0100

    Day 12: Still ugly, but faster

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,152 +1,162 @@
 use itertools::Itertools;
-use rayon::iter::IntoParallelRefIterator;
-use rayon::prelude::*;
-use std::collections::{BinaryHeap, HashMap};
-use std::str::FromStr;
+use std::collections::{HashSet, VecDeque};
+use std::iter::repeat;
 
 const INPUT: &str = include_str!("input.txt");
 const INPUT_TEST: &str = include_str!("input.test.txt");
 
 fn main() {
-    assert_eq!(solve(INPUT_TEST), Some(31));
-    println!("Part A: {:#?}", solve(INPUT));
+    assert_eq!(solve_a(INPUT_TEST), Some(31));
+    println!("Part A: {:#?}", solve_a(INPUT));
 
     assert_eq!(solve_b(INPUT_TEST), Some(29));
     println!("Part B: {:#?}", solve_b(INPUT));
 }
 
-fn solve(input: &str) -> Option<usize> {
-    let map: Map = input.parse().unwrap();
+fn solve_a(input: &str) -> Option<usize> {
+    let chars = input.lines().flat_map(|l| l.chars()).collect_vec();
+    let from = chars.iter().position(|&c| c == 'S').unwrap();
+    let to = chars.iter().position(|&c| c == 'E').unwrap();
 
-    shortest_path(map)
+    let grid = parse(input);
+
+    bfs(
+        from,
+        grid.clone(),
+        |index, _| index == to,
+        |node| {
+            grid.neighbours(node)
+                .iter()
+                .filter(|(_, h)| *h <= grid.values[node] + 1)
+                .map(|x| x.0)
+                .collect_vec()
+        },
+    )
+    .map(|path| path.len())
 }
 
 fn solve_b(input: &str) -> Option<usize> {
-    let map: Map = input.parse().unwrap();
-    let maps: Vec<_> = map
-        .elevations
-        .iter()
-        .filter(|(point, &elevation)| elevation == 1)
-        .map(|(point, elevation)| Map {
-            start: point.clone(),
-            end: map.end.clone(),
-            elevations: map.elevations.clone(),
-        })
-        .collect_vec();
+    let chars = input.lines().flat_map(|l| l.chars()).collect_vec();
+    let from = chars.iter().position(|&c| c == 'E').unwrap();
 
-    maps.into_par_iter().filter_map(shortest_path).min()
+    let grid = parse(input);
+
+    bfs(
+        from,
+        grid.clone(),
+        |_, height| height == 1,
+        |node| {
+            grid.neighbours(node)
+                .iter()
+                .filter(|(_, h)| *h >= grid.values[node] - 1)
+                .map(|x| x.0)
+                .collect_vec()
+        },
+    )
+    .map(|path| path.len())
 }
 
-fn shortest_path(map: Map) -> Option<usize> {
-    println!(
-        "Running shortest path with start: {}, {}",
-        map.start.x, map.start.y
-    );
-    let mut distances: HashMap<Point, usize> = HashMap::new();
-    let mut queue: BinaryHeap<(isize, usize, Point)> = BinaryHeap::new();
+fn bfs<T, GoalFn, NeighboursFn>(
+    start: usize,
+    grid: Grid<T>,
+    goal: GoalFn,
+    neighbours: NeighboursFn,
+) -> Option<Vec<usize>>
+where
+    T: Copy,
+    GoalFn: Fn(usize, T) -> bool,
+    NeighboursFn: Fn(usize) -> Vec<usize>,
+{
+    let mut queue: VecDeque<usize> = VecDeque::new();
+    let mut trace: Vec<usize> = repeat(usize::MAX).take(grid.values.len()).collect();
+    let mut seen: HashSet<usize> = HashSet::new();
 
-    distances.insert(Point::new(0, 0), 0);
-    queue.push((-1, 0, map.start.clone()));
+    queue.push_back(start);
+    seen.insert(start);
 
-    while let Some((elevation, steps, point)) = queue.pop() {
-        if point == map.end {
-            return Some(steps);
-        }
-
-        if steps > *distances.get(&point).unwrap_or(&usize::MAX) {
-            continue;
-        }
-
-        for (height, point) in map.valid_neighbours(point) {
-            if steps + 1 < *distances.get(&point).unwrap_or(&usize::MAX) {
-                queue.push((-(height as isize), steps + 1, point.clone()));
-                distances.insert(point, steps + 1);
+    let mut current_node: usize;
+    loop {
+        if let Some(node) = queue.pop_front() {
+            current_node = node;
+            if goal(node, grid.values[node]) {
+                break;
             }
-        }
-    }
 
-    None
-}
-
-#[derive(Debug)]
-struct Map {
-    start: Point,
-    end: Point,
-    elevations: HashMap<Point, usize>,
-}
-
-impl FromStr for Map {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut elevations: HashMap<Point, usize> = HashMap::new();
-        let mut start = Point::new(0, 0);
-        let mut end = Point::new(0, 0);
-        for (y, line) in s.lines().enumerate() {
-            for (x, char) in line.chars().enumerate() {
-                let elevation = match char {
-                    'S' => 'a' as usize,
-                    'E' => 'z' as usize,
-                    _ => char as usize,
-                } - 96;
-
-                let point = Point::new(x as i32, y as i32);
-                elevations.insert(point.clone(), elevation);
-
-                match char {
-                    'S' => {
-                        start = point;
-                    }
-                    'E' => {
-                        end = point;
-                    }
-                    _ => {}
+            for index in neighbours(node) {
+                if seen.contains(&index) {
+                    continue;
                 }
+
+                trace[index] = node;
+                queue.push_back(index);
+                seen.insert(index);
             }
+        } else {
+            return None;
         }
-
-        Ok(Map {
-            start,
-            end,
-            elevations,
-        })
     }
+
+    let mut index = current_node;
+    let mut path = vec![index];
+    while trace[index] != start {
+        index = trace[index];
+        path.push(index);
+    }
+
+    path.reverse();
+
+    Some(path)
 }
 
-impl Map {
-    pub fn valid_neighbours(&self, point: Point) -> Vec<(usize, Point)> {
-        let current_elevation = *self.elevations.get(&point).unwrap();
+#[derive(Clone)]
+struct Grid<T> {
+    values: Vec<T>,
+    width: usize,
+}
 
-        return [
-            Point::new(-1, 0),
-            Point::new(1, 0),
-            Point::new(0, 1),
-            Point::new(0, -1),
+impl<T> Grid<T> {
+    fn neighbours(&self, i: usize) -> Vec<(usize, T)>
+    where
+        T: Clone,
+    {
+        [
+            if i == 0 { None } else { Some(i - 1) },
+            if i > self.values.len() - 2 {
+                None
+            } else {
+                Some(i + 1)
+            },
+            if i < self.width {
+                None
+            } else {
+                Some(i - self.width)
+            },
+            if i > self.values.len() - self.width - 1 {
+                None
+            } else {
+                Some(i + self.width)
+            },
         ]
-        .iter()
-        .map(|x| x.add(&point))
-        .filter_map(|x| {
-            self.elevations
-                .get(&x)
-                .filter(|&&x| x <= current_elevation + 1)
-                .map(|&h| (h, x))
-        })
-        .collect_vec();
+        .into_iter()
+        .filter_map(|j| j.map(|k| (k, self.values[k].clone())))
+        .collect()
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Hash, Clone, Ord, PartialOrd)]
-struct Point {
-    x: i32,
-    y: i32,
+fn parse(input: &str) -> Grid<isize> {
+    Grid {
+        values: input
+            .lines()
+            .flat_map(|l| l.chars().map(elevation))
+            .collect_vec(),
+        width: input.lines().next().unwrap().len(),
+    }
 }
 
-impl Point {
-    pub fn new(x: i32, y: i32) -> Self {
-        Point { x, y }
-    }
-
-    fn add(&self, other: &Self) -> Self {
-        Point::new(self.x + other.x, self.y + other.y)
+fn elevation(c: char) -> isize {
+    match c {
+        'S' => 1,
+        'E' => 26,
+        _ => c as isize - 96,
     }
 }
