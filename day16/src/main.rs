bc4671242c85027bc2fb604083aadb0f48478d99
Jan Kramer <jan@jankramer.eu>
2022-12-17 05:13:43 +0100

    Day 16: Order by total value

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -9,13 +9,13 @@ const INPUT: &str = include_str!("input.txt");
 const INPUT_TEST: &str = include_str!("input.test.txt");
 
 fn main() {
-    assert_eq!(solve(INPUT_TEST), 1651);
-    println!("Part A: {}", solve(INPUT));
+    assert_eq!(solve(INPUT_TEST, 30), 1651);
+    println!("Part A: {}", solve(INPUT, 30));
 }
 
 const MAGIC_NUMBER: usize = 300000;
 
-fn solve(input: &str) -> usize {
+fn solve(input: &str, minutes_left: usize) -> usize {
     let valves_input: Vec<ValveInput> =
         input.lines().sorted().map(|l| l.parse().unwrap()).collect();
 
@@ -35,7 +35,7 @@ fn solve(input: &str) -> usize {
         .collect();
 
     let mut volcanos: Vec<Volcano> = Vec::with_capacity(MAGIC_NUMBER);
-    volcanos.push(Volcano::new());
+    volcanos.push(Volcano::new(minutes_left));
     let mut queue: BinaryHeap<Volcano> = BinaryHeap::with_capacity(10 * MAGIC_NUMBER);
 
     for i in 1..=30 {
@@ -70,15 +70,17 @@ struct Volcano {
     current_valve: usize,
     total_pressure: usize,
     total_flow_rate: usize,
+    minutes_left: usize,
 }
 
 impl Volcano {
-    fn new() -> Self {
+    fn new(minutes_left: usize) -> Self {
         Volcano {
             open: HashSet::new(),
             current_valve: 0,
             total_pressure: 0,
             total_flow_rate: 0,
+            minutes_left,
         }
     }
 
@@ -106,6 +108,10 @@ impl Volcano {
             queue.push(new_with_next_as_current);
         }
     }
+
+    fn value(&self) -> usize {
+        self.total_pressure + self.total_flow_rate * self.minutes_left
+    }
 }
 impl PartialOrd<Self> for Volcano {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
@@ -115,9 +121,7 @@ impl PartialOrd<Self> for Volcano {
 
 impl Ord for Volcano {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.total_flow_rate
-            .cmp(&other.total_flow_rate)
-            .then(self.total_pressure.cmp(&other.total_pressure))
+        self.value().cmp(&other.value())
     }
 }
 
